1e96b41fbdcbf1cdfa4a4cd7ad3da72c9aeb9f16
Matheus Lino <matheuslino86@gmail.com.br>
2023-10-23 08:49:08 -0400

    feat: :sparkles: Registerkey usecase

---
+++ application/usecase/pix.go
@@ -0,0 +1,27 @@
+package usecase
+
+import "github.com/poring86/codepix-go/domain/model"
+
+type PixUseCase struct {
+	PixKeyRepository model.PixKeyRepositoryInterface
+}
+
+func (p *PixUseCase) RegisterKey(key string, kind string, accountId string) (*model.PixKey, error) {
+	account, err := p.PixKeyRepository.FindAccount(accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	pixKey, err := model.NewPixKey(kind, account, key)
+	if err != nil {
+		return nil, err
+	}
+
+	p.PixKeyRepository.RegisterKey(pixKey)
+
+	if pixKey.ID == "" {
+		return nil, err
+	}
+
+	return pixKey, nil
+}
